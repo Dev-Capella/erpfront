55f5403eadca2988b1b929cd1113a05a60077082
mahmutcopoglu <mahmutcopoglu@gmail.com>
2023-12-10 00:43:56 +0300

    main

--- src/app/modules/item-type/components/data-item-type/data-item-type.component.ts
+++ src/app/modules/item-type/components/data-item-type/data-item-type.component.ts
@@ -1,13 +1,16 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { ItemTypeService } from '../../services/item-type.service';
+import { UnitOfMeasureService } from '../../../unit-of-measure/services/unit-of-measure.service';
+import { BaseComponent } from '../../../../core/components/base/base.component';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-data-item-type',
   templateUrl: './data-item-type.component.html',
   styleUrl: './data-item-type.component.scss'
 })
-export class DataItemTypeComponent implements OnInit {
+export class DataItemTypeComponent extends BaseComponent implements OnInit {
   itemTypeForm: FormGroup;
   keepLeft: boolean = true;
   keepRight: boolean = true;
@@ -23,11 +26,34 @@ export class DataItemTypeComponent implements OnInit {
     {code: 'PATTERN', name: 'Pattern'},
     {code: 'COST_ELEMENT', name: 'Cost element'},
   ]
-  constructor(private formBuilder: FormBuilder,
-    private itemTypeService:ItemTypeService) {
-    
+  structures: any[] = [
+    {code: 'NA', name: 'N/A'},
+    {code: 'FINISHED_ITEM', name: 'Finished item'},
+    {code: 'SEMI_FINISHED_ITEM', name: 'Semi finished item'},
+    {code: 'RAW_MATERIAL', name: 'Raw material'},
+  ]
+  statusAlloweds: any[] = [
+    {code: 'NA', name: 'N/A'},
+    {code: 'ACTIVE', name: 'Active'},
+    {code: 'SUSPENDED_ACTIVE', name: 'Suspended+Active'},
+    {code: 'SUSPENDED_APPROVED_ACTIVE', name: 'Suspended+Approved+Active'},
+  ]
+  basePackagingTypes: any[] = [
+    {code: 'PRIMARY', name: 'Primary'},
+    {code: 'SECONDARY', name: 'Secondary'}
+  ]
+  uomList: any[] = []
+  packagingUOMList: any[] = []
+  secondaryUOMVisible: boolean = false;
+  packagingUOMVisible: boolean = false;
+  constructor(
+    spinner: NgxSpinnerService,
+    private formBuilder: FormBuilder,
+    private itemTypeService:ItemTypeService,
+    private unitOfMeasureService: UnitOfMeasureService) {
+    super(spinner)
   }
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
     
     this.itemTypeForm = this.formBuilder.group({
       id: new FormControl(null),
@@ -41,8 +67,19 @@ export class DataItemTypeComponent implements OnInit {
       valid: new FormControl(false),
       managedByBox: new FormControl(false),
       handleComponentStatus: new FormControl(false),
+      structure: new FormControl(null),
+      statusAllowed: new FormControl(null),
+      primaryUOM: new FormControl(null),
+      secondaryUnitControlled: new FormControl(false),
+      secondaryUOM: new FormControl(null),
+      secondaryConversionFactor: new FormControl(null),
+      packagingUnitControlled: new FormControl(false),
+      baseUoMPackagingType: new FormControl(null),
+      packagingUOM: new FormControl(null),
+      packagingConversionFactor: new FormControl(null),
     });
 
+  
     this.itemTypeService.selectedData.subscribe(result=>{
       if(!!result){
         this.itemTypeForm.setValue(result);
@@ -63,6 +100,8 @@ export class DataItemTypeComponent implements OnInit {
     this.itemTypeService.keepRight.subscribe(value => {
       this.keepRight=value
     })
+
+    await this.getUOMList();
   }
 
   @Output() itemTypeFormGroupValue : EventEmitter<any> = new EventEmitter();
@@ -75,5 +114,32 @@ export class DataItemTypeComponent implements OnInit {
     this.keepRight = false;
     this.itemTypeService.keepRight.next(this.keepRight);
     this.itemTypeService.selectedData.next(null);
+  }
+
+  async getUOMList(){
+    this.showSpinner();
+    this.uomList = await this.unitOfMeasureService.getUnitOfMeasures(()=> this.hideSpinner());
+    this.packagingUOMList = this.uomList.filter(x=> x.unitOfMeasureType == 'PACKAGING')
+  }
+
+  changeSecondaryUoM(event){
+    if(event.checked){
+      this.secondaryUOMVisible = true;
+    }else{
+      this.secondaryUOMVisible = false;
+      this.itemTypeForm.get('secondaryUOM').reset();
+      this.itemTypeForm.get('secondaryConversionFactor').reset();
+    }
+  }
+
+  changePackagingUoM(event){
+    if(event.checked){
+      this.packagingUOMVisible = true;
+    }else{
+      this.packagingUOMVisible = false;
+      this.itemTypeForm.get('baseUoMPackagingType').reset();
+      this.itemTypeForm.get('packagingUOM').reset();
+      this.itemTypeForm.get('packagingConversionFactor').reset();
+    }
   }
 }
