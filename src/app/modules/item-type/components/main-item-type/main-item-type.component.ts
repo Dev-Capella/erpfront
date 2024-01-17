97567eafa84e7d23f28c691cd842348daeccbec9
mahmutcopoglu <mahmutcopoglu@gmail.com>
2024-01-18 00:21:49 +0300

    main

--- src/app/modules/item-type/components/main-item-type/main-item-type.component.ts
+++ src/app/modules/item-type/components/main-item-type/main-item-type.component.ts
@@ -14,23 +14,6 @@ import { Router } from '@angular/router';
   styleUrl: './main-item-type.component.scss'
 })
 export class MainItemTypeComponent extends BaseComponent implements OnInit {
-
-  menus: MenuItem[] = [
-  {
-      label: 'Update',
-      icon: 'pi pi-refresh',
-      command: () => {
-        this.detail(this.activeItem);
-      }
-    },
-    {
-      label: 'Delete',
-      icon: 'pi pi-times',
-      command: async () => {
-        await this.delete(this.activeItem);
-      }
-    }
-  ];
   activeItem: any;
   items: MenuItem[] = [{
     label: 'Options',
@@ -61,115 +44,6 @@ export class MainItemTypeComponent extends BaseComponent implements OnInit {
     private confirmationService: ConfirmationService) {
     super(spinner);
   }
-  // ngOnInit(): void {
-  //   this.itemTypeService.keepLeft$.subscribe(result=>{
-  //       this.keepLeft = result;
-  //   });
-
-  //   this.itemTypeService.keepRight$.subscribe(result=>{
-  //     this.keepRight = result;
-  //     this.menus.filter(item=>item.id==MenuItemActions.NEW)[0].disabled=this.keepRight;
-  //     this.menus.filter(item=>item.id==MenuItemActions.SAVE)[0].disabled=!this.keepRight;
-  //     this.menus.filter(item=>item.id==MenuItemActions.SAVE_AND_NEW)[0].disabled=!this.keepRight;
-  //     this.menus = [...this.menus];
-  //   });
-
-  //   this.itemTypeService.selectedData$.subscribe(result=>{
-  //     if(!!result){
-  //       this.selectedData = result;
-  //       this.menus.filter(item=>item.id==MenuItemActions.DELETE)[0].disabled=false;
-  //       this.functionMenus = this.functionMenus.map(item => {return {...item, disabled: false}})       
-  //     }else{
-  //       this.menus.filter(item=>item.id==MenuItemActions.DELETE)[0].disabled=true;
-  //       this.functionMenus = this.functionMenus.map(item => {return {...item, disabled: true}}) 
-  //     }
-  //     this.menus = [...this.menus];
-  //   });
-
-  //   this.itemTypeService.visibleQualityTemplate$.subscribe(result=>{
-  //       this.visibleQualityTemplate = result;
-  //   });
-
-  //   this.itemTypeService.visibleSubcodeTemplate$.subscribe(result=>{
-  //       this.visibleSubcodeTemplate = result;
-  //   });
-
-  //   this.itemTypeService.visibleBoMTemplate$.subscribe(result=>{
-  //       this.visibleBoMTemplate = result;
-  //   });
-
-  //   this.itemTypeService.visibleRoutingTemplate$.subscribe(result=>{
-  //     this.visibleRoutingTemplate = result;
-  //   });
-    
-  // }
-
-  // async onSubmit(){
-  //   var request = {
-  //     id: this.formData.value?.id,
-  //     code: this.formData.value?.code,
-  //     shortText: this.formData.value?.shortText,
-  //     longText: this.formData.value?.longText,
-  //     searchText:this.formData.value?.searchText,
-  //     itemNature: this.formData.value?.itemNature,
-  //     maxCodeLength: this.formData.value?.maxCodeLength,
-  //     sellingType: this.formData.value?.sellingType,
-  //     valid: this.formData.value?.valid,
-  //     managedByBox: this.formData.value?.managedByBox,
-  //     handleComponentStatus: this.formData.value?.handleComponentStatus,
-  //     structure: this.formData.value?.structure,
-  //     statusAllowed: this.formData.value?.statusAllowed,
-  //     primaryUOM: {code: this.formData.value?.primaryUOM?.code },
-  //     secondaryUnitControlled: this.formData.value?.secondaryUnitControlled !== null,
-  //     secondaryUOM: this.formData.value?.secondaryUOM?.code ? {code: this.formData.value?.secondaryUOM?.code } : null,
-  //     secondaryConversionFactor: this.formData.value?.secondaryConversionFactor,
-  //     packagingUnitControlled: this.formData.value?.packagingUnitControlled !== null,
-  //     baseUoMPackagingType: this.formData.value?.baseUoMPackagingType,
-  //     packagingUOM: this.formData.value?.packagingUOM?.code ? {code: this.formData.value?.packagingUOM?.code } : null,
-  //     packagingConversionFactor: this.formData.value?.packagingConversionFactor,
-  //     qualityControlled: this.formData.value?.qualityControlled,
-  //     lotControlled: this.formData.value?.lotControlled,
-  //     containerControlled: this.formData.value?.containerControlled,
-  //     elementControlled: this.formData.value?.elementControlled,
-  //     projectControlled: this.formData.value?.projectControlled,
-  //     customerControlled: this.formData.value?.customerControlled,
-  //     supplierControlled: this.formData.value?.supplierControlled
-  //   }
-
-  //   this.showSpinner();
-  //   await this.itemTypeService.saveItemType(request,()=> this.hideSpinner());
-  //   this.messageService.add({severity:'success', summary:'İşlem Sonucu', detail:'Item Type başarıyla Kaydedilmiştir.'});
-  //   this.itemTypeService.isRefreshList.next(true);
-  //   this.formData.reset();
-  // }
-
-  // async deleteItemType(){
-  //   this.showSpinner();
-  //   await this.itemTypeService.deleteItemTypeByCode(this.selectedData.code, ()=> this.hideSpinner());
-  //   this.messageService.add({severity:'success', summary:'İşlem Sonucu', detail:'UoM başarıyla silinmiştir.'});
-  //   this.itemTypeService.isRefreshList.next(true);
-  //   this.itemTypeService.keepRight.next(false);
-  // }
-
-  // getFormValue(event){
-  //   this.formData = event;
-  // }
-
-  // openSubcodeTemplate(){
-  //   this.visibleSubcodeTemplate = true;
-  // }
-
-  // openQualityTemplate(){
-  //   this.visibleQualityTemplate = true;
-  // }
-
-  // openBoMTemplate(){
-  //   this.visibleBoMTemplate = true;
-  // }
-
-  // openRoutingTemplate(){
-  //   this.visibleRoutingTemplate = true;
-  // }
 
   async ngOnInit(): Promise<void> {
     await this.getItemTypes();
