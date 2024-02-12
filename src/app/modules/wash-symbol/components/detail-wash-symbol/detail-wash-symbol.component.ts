6264bb4c53f7d1f1317ff0132456ed13ff4e8e7d
mahmutcopoglu <mahmutcopoglu@gmail.com>
2024-02-12 22:30:17 +0300

    main

--- src/app/modules/wash-symbol/components/detail-wash-symbol/detail-wash-symbol.component.ts
+++ src/app/modules/wash-symbol/components/detail-wash-symbol/detail-wash-symbol.component.ts
@@ -1,10 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BaseComponent } from '../../../../core/components/base/base.component';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { ActivatedRoute, Router } from '@angular/router';
+import { WashSymbolCategoryService } from '../../../wash-symbol-category/services/wash-symbol-category.service';
+import { WashSymbolService } from '../../services/wash-symbol.service';
 
 @Component({
   selector: 'app-detail-wash-symbol',
   templateUrl: './detail-wash-symbol.component.html',
   styleUrl: './detail-wash-symbol.component.scss'
 })
-export class DetailWashSymbolComponent {
+export class DetailWashSymbolComponent extends BaseComponent implements OnInit {
+  washSymbolForm: FormGroup;
+  activeTab: number = 0;
+  activeMenu: number = 0;
+  code: string;
+  descriptionText: string = "";
+  constructor(spinner: NgxSpinnerService,
+    private formBuilder: FormBuilder,
+    private washSymbolService:WashSymbolService,
+    private washSymbolCategoryService:WashSymbolCategoryService,
+    private confirmationService: ConfirmationService,
+    private messageService: MessageService,
+    private route: ActivatedRoute,
+    private router: Router) {
+    super(spinner);
+    this.code = this.route.snapshot.params['code']
+  }
+  async ngOnInit(): Promise<void> {
+    this.washSymbolForm = this.formBuilder.group({
+      id: new FormControl(null),
+      code: new FormControl(null),
+      shortText: new FormControl(null),
+      longText: new FormControl(null),
+      searchText: new FormControl(null),
+    });
+    await this.getWashSymbolByCode();
+  }
 
+  get formControls(){
+    return this.washSymbolForm.controls;
+  }
+
+  async getWashSymbolByCode(){
+    this.showSpinner();
+    var result = await this.washSymbolService.getWashSymbolByCode(this.code, ()=> this.hideSpinner());
+    this.washSymbolForm.setValue(result);
+    this.descriptionText = `Short: ${result.shortText ? result.shortText : "-"}, Long: ${result.longText ? result.longText : "-"}, Search: ${result.searchText ? result.searchText : "-"}`
+  }
+
+
+  save(value){
+    if(this.washSymbolForm.invalid){
+      return;
+    }
+    this.confirmationService.confirm({
+      key: 'save-wash-symbol',
+      header: 'Transaction Confirmation',
+      message: 'The wash symbol is being recorded. Are you sure?',
+      accept: async () => {
+        var request = {
+          id: value?.id,
+          code: value?.code,
+          longText: value?.longText,
+          shortText: value?.shortText,
+          searchText: value?.searchText,
+        }
+        this.showSpinner();
+        await this.washSymbolService.saveWashSymbol(request, ()=> this.hideSpinner());
+        this.messageService.add({severity:'success', summary:'Transaction Result', detail:'Wash symbol has been saved successfully.'});
+        this.router.navigate(['/wash-symbol-list'])
+      }
+  });
+   
+  }
+
+  goBack(){
+    this.router.navigate(['/wash-symbol-list'])
+  }
 }
--- src/app/modules/wash-symbol/components/new-wash-symbol/new-wash-symbol.component.ts
+++ src/app/modules/wash-symbol/components/new-wash-symbol/new-wash-symbol.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BaseComponent } from '../../../../core/components/base/base.component';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { Router } from '@angular/router';
+import { WashSymbolCategoryService } from '../../../wash-symbol-category/services/wash-symbol-category.service';
+import { WashSymbolService } from '../../services/wash-symbol.service';
 
 @Component({
   selector: 'app-new-wash-symbol',
   templateUrl: './new-wash-symbol.component.html',
   styleUrl: './new-wash-symbol.component.scss'
 })
-export class NewWashSymbolComponent {
+export class NewWashSymbolComponent extends BaseComponent implements OnInit{
+  washSymbolForm: FormGroup;
+  activeTab: number = 0;
+  activeMenu: number = 0;
+  constructor(spinner: NgxSpinnerService,
+    private formBuilder: FormBuilder,
+    private washSymbolService:WashSymbolService,
+    private washSymbolCategoryService:WashSymbolCategoryService,
+    private confirmationService: ConfirmationService,
+    private messageService: MessageService,
+    private router: Router) {
+    super(spinner);
+    
+  }
+  ngOnInit(): void {
+    this.washSymbolForm = this.formBuilder.group({
+      id: new FormControl(null),
+      code: new FormControl(null),
+      shortText: new FormControl(null),
+      longText: new FormControl(null),
+      searchText: new FormControl(null),
+    });
+  }
 
+  get formControls(){
+    return this.washSymbolForm.controls;
+  }
+
+  save(value){
+    if(this.washSymbolForm.invalid){
+      return;
+    }
+    this.confirmationService.confirm({
+      key: 'save-wash-symbol',
+      header: 'Transaction Confirmation',
+      message: 'The wash symbol is being recorded. Are you sure?',
+      accept: async () => {
+        var request = {
+          longText: value?.longText,
+          shortText: value?.shortText,
+          searchText: value?.searchText,
+        }
+        this.showSpinner();
+        await this.washSymbolService.saveWashSymbol(request, ()=> this.hideSpinner());
+        this.messageService.add({severity:'success', summary:'Transaction Result', detail:'Wash symbol has been saved successfully.'});
+        this.router.navigate(['/wash-symbol-list'])
+      }
+  });
+   
+  }
+
+  goBack(){
+    this.router.navigate(['/wash-symbol-list'])
+  }
 }
