c3e753dd60bf8c7312f0851238bd0803e5272013
mahmutcopoglu <mahmutcopoglu@gmail.com>
2024-02-17 14:15:25 +0300

    main

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 import { PageRoutes } from "../app/shared/enums/page-routes.enum";
 
 export const environment = {
-    hostRoot: PageRoutes.LocalHost,
+    hostRoot: PageRoutes.LocalHost + PageRoutes.Webapp + PageRoutes.Api,
     requestRoot: PageRoutes.LocalHost + PageRoutes.Webapp + PageRoutes.Api + PageRoutes.Version,
     scheduledTaskRoot: PageRoutes.CronHost + PageRoutes.Cron + PageRoutes.Api + PageRoutes.Version,
 }
